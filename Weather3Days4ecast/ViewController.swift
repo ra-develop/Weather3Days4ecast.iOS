9bc40c09cb5ee965d96d82284bdaa7c0a9c396d6
R.A <rashid@rescona.kz>
2020-03-17 23:40:54 +0100

    Initial commit

--- Weather3Days4ecast/ViewController.swift
+++ Weather3Days4ecast/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backGroundImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Control of background image animation of main screen 
+        UIView.animate(withDuration: 60.0, delay: 1.0, options: [.curveEaseOut, .curveEaseIn, .autoreverse,] , animations: {
+            var backGroundImafeFrame = self.backGroundImage.frame
+            backGroundImafeFrame.origin.x += backGroundImafeFrame.size.width - self.view.bounds.width
+            self.backGroundImage.frame = backGroundImafeFrame
+        })
+        
+    }
+    
 }
 
