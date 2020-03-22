5f0482ebe46cc74ebebca24ea11267fd535e7513
R.A <rashid@rescona.kz>
2020-03-22 12:19:55 +0100

    1. Added table view preparation construction (tableview function and Forecast Cell class) 2. Added request and parsing forecast for selected city

---
+++ Weather3Days4ecast/View Controllers/Cells/ForecastTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ForecastTableViewCell.swift
+//  Weather3Days4ecast
+//
+//  Created by RA on 22/03/2020.
+//  Copyright © 2020 RA. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
--- Weather3Days4ecast/View Controllers/DetailViewController.swift
+++ Weather3Days4ecast/View Controllers/DetailViewController.swift
@@ -13,8 +13,14 @@ import CoreLocation
 import SwiftVideoBackground
 
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    fileprivate var weatherSama: Weathersama!
+    fileprivate var forecastModel: ForecastModel!
     var selectedCity : WeatherModel!
     var selectedCityTimezone : Int = 0
     
@@ -53,7 +59,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        getForecastByCityId()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -102,8 +108,83 @@ class DetailViewController: UIViewController {
 
         sunsetLabel.text = getTimeByInterval(timeZone: timeZone!, interval: selectedCity.sys.sunset)
         
-        
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // Getting forecast for selected city
+    func getForecastByCityId() {
+        // Setup and request list of forecast of cities
+        weatherSama = Weathersama(appId: APP_ID, temperature: TEMPERATURE_TYPES.Celcius, language: LANGUAGES.English, dataResponse: DATA_RESPONSE.JSON)
+        weatherSama.weatherByCityId(cityId: selectedCity.cityId, requestType: .Forecast) { (isSuccess, description, classModel) -> () in
+            if isSuccess {
+                print("response json : \(description)")
+                self.forecastModel = classModel as? ForecastModel
+                self.tableView.reloadData()
+            } else {
+                print("response error : \(description)")
+            }
+        }
+        
+    }
 }
 
