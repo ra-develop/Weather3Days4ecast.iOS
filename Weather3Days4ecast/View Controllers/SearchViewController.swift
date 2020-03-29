ae3afd5470a55f98918d1d5ad05121ec459fe888
R.A <rashid@rescona.kz>
2020-03-29 22:40:30 +0200

    1. Setup datasource for search table view 2. Setup UISearchBar delegate function 3. Fix URL encoding (percent Escaped  RFC 3986) 4. Prototyping search table view

--- Weather3Days4ecast/View Controllers/SearchViewController.swift
+++ Weather3Days4ecast/View Controllers/SearchViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import Weathersama
 import Alamofire
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate,  UISearchBarDelegate {
+    
     
     var searchController: UISearchController!
     
-    var searchData: SearchCitiesResult!
+    var searchData = SearchCitiesResult()
     
     
     @IBOutlet var searchBar: UISearchBar!
@@ -26,24 +27,27 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        findByName(searchName: "Milan") { (isSuccess, description, classModel) -> () in
-            if isSuccess && description != "" {
-                // you can user response json or class model
-                // print("response json : \(description)")
-                self.searchData = classModel as? SearchCitiesResult
-                for item in self.searchData.list {
-                    print("Found city: \(String(item.cityName)) \(String(item.sys.country))")
-                }
-                print("Count of found cities: \(self.searchData.count)")
-            } else {
-                self.present(alertResponseError(description: description), animated: true, completion: nil)
-            }
-        }
-
-
+        searchBar.delegate = self
+        searchBar.autocapitalizationType = .none
+        
     }
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchData.list.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath)
+        
+        cell.textLabel?.text = self.searchData.list[indexPath.row].cityName
+        
+        return cell
+    }
     
     /*
     // MARK: - Navigation
@@ -68,14 +72,58 @@ class SearchViewController: UIViewController {
     }
 
     internal func requestAPI(url: String, completion: @escaping(Bool, String, AnyObject?) -> ()) {
-        Alamofire.request(url).responseJSON { response in
+        
+        Alamofire.request(url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!).responseJSON { response in
             if let data = response.data, let utf8Text = String(data: data, encoding: .utf8)  {
                     let resultObj = SearchCitiesResult()
+                
                     resultObj.parseJSON(jsonSerialized: response.result.value as AnyObject)
                     completion(true, utf8Text, resultObj)
             } else {
                 completion(false, response.error.debugDescription, nil)
             }
+        }
+    }
+    
+    
+    // MARK: Setup datasource
+    internal func getSearchData(searchName: String) {
+       findByName(searchName: searchName) { (isSuccess, description, classModel) -> () in
+            if isSuccess && description != "" {
+                // you can user response json or class model
+                // print("response json : \(description)")
+                self.searchData = (classModel as? SearchCitiesResult)!
+                for item in self.searchData.list {
+                    print("Found city: \(String(item.cityName)) ,\(String(item.sys.country))")
+                }
+                print("Count of found cities from web site: \(self.searchData.count)")
+                print("Count of found cities in memory: \(self.searchData.list.count)")
+                self.tableView.reloadData()
+            } else {
+                self.present(alertResponseError(description: description), animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    // MARK: UISearchbar delegate
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        // Strip out all the leading and trailing spaces.
+        let whitespaceCharacterSet = CharacterSet.whitespaces
+        let searchItems = searchBar.text!.trimmingCharacters(in: whitespaceCharacterSet)
+                
+        if searchItems.count == 0 || searchItems.last == " " {
+            self.tableView.reloadData()
+        } else {
+            getSearchData(searchName: searchItems)
+            
         }
     }
 
